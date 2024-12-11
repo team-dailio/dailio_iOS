9a09a25ec29f1f6350cdbce8d64a6a8518848acc
circle0802 <hawon9781@gmail.com>
2024-12-12 08:42:32 +0900

    setting :: tuist 적용

---
+++ Targets/DailioIOSKit/Tests/DailioIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DailioIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("DailioIOSKit", "DailioIOSKit")
+    }
+}
---
+++ Targets/DailioIOSUI/Tests/DailioIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DailioIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("DailioIOSUI", "DailioIOSUI")
+    }
+}
