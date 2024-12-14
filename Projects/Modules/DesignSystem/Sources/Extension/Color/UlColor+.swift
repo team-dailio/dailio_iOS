1066f7b0e3ace1276103910f89760cedea93ae3c
circle0802 <hawon9781@gmail.com>
2024-12-14 15:30:53 +0900

    chore :: UIColor+ 오류 수정

--- Projects/Modules/DesignSystem/Sources/Extension/Color/UlColor+.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/Color/UlColor+.swift
@@ -1,19 +1,19 @@
 import UIKit
 
 public extension UIColor {
-    static let primary100 = DesignSystemAsset.primary100.color
-    static let primary200 = DesignSystemAsset.primary200.color
-    static let primary300 = DesignSystemAsset.primary300.color
-    static let primary400 = DesignSystemAsset.primary400.color
-    static let primary500 = DesignSystemAsset.primary500.color
-    static let gray100 = DesignSystemAsset.gray100.color
-    static let gray200 = DesignSystemAsset.gray200.color
-    static let gray300 = DesignSystemAsset.gray300.color
-    static let gray400 = DesignSystemAsset.gray400.color
-    static let gray500 = DesignSystemAsset.gray500.color
-    static let error = DesignSystemAsset.error.color
-    static let success = DesignSystemAsset.success.color
-    static let info = DesignSystemAsset.info.color
-    static let black = DesignSystemAsset.black.color
-    static let white = DesignSystemAsset.white.color
+    static let primary100 = DesignSystemAsset.Colors.primary100.color
+    static let primary200 = DesignSystemAsset.Colors.primary200.color
+    static let primary300 = DesignSystemAsset.Colors.primary300.color
+    static let primary400 = DesignSystemAsset.Colors.primary400.color
+    static let primary500 = DesignSystemAsset.Colors.primary500.color
+    static let gray100 = DesignSystemAsset.Colors.gray100.color
+    static let gray200 = DesignSystemAsset.Colors.gray200.color
+    static let gray300 = DesignSystemAsset.Colors.gray300.color
+    static let gray400 = DesignSystemAsset.Colors.gray400.color
+    static let gray500 = DesignSystemAsset.Colors.gray500.color
+    static let error = DesignSystemAsset.Colors.error.color
+    static let success = DesignSystemAsset.Colors.success.color
+    static let info = DesignSystemAsset.Colors.info.color
+    static let black = DesignSystemAsset.Colors.black.color
+    static let white = DesignSystemAsset.Colors.white.color
 }
