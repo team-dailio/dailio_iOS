fbe574b7b2e39b223bd002b050047b5baee2c076
circle0802 <hawon9781@gmail.com>
2024-12-14 15:29:52 +0900

    setting :: Icon UIImage+에 추가

--- Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 public extension UIImage {
-    
+    static let filledHeart = DesignSystemAsset.Images.filledheart.image
+    static let outlinedHeart = DesignSystemAsset.Images.outlinedHeart.image
+    static let filledHome = DesignSystemAsset.Images.filledHome.image
+    static let outlinedHome = DesignSystemAsset.Images.outlinedHome.image
+    static let filledCalender = DesignSystemAsset.Images.filledCalendar.image
+    static let outlinedCalender = DesignSystemAsset.Images.outlinedCalendar.image
+    static let filledFeed = DesignSystemAsset.Images.filledFeed.image
+    static let outlinedFeed = DesignSystemAsset.Images.outlinedFeed.image
+    static let checklistLtl = DesignSystemAsset.Images.checklistRtl.image
+    static let checklistLtr = DesignSystemAsset.Images.checklistLtr.image
+    static let filledDashboard = DesignSystemAsset.Images.filledDashboard.image
+    static let outlinedDashboard = DesignSystemAsset.Images.outlinedDashboard.image
+    static let setting = DesignSystemAsset.Images.setting.image
+    static let menu = DesignSystemAsset.Images.menu.image
+    static let right = DesignSystemAsset.Images.right.image
+    static let left = DesignSystemAsset.Images.left.image
+    static let down = DesignSystemAsset.Images.down.image
+    static let up = DesignSystemAsset.Images.up.image
+    static let dropdown = DesignSystemAsset.Images.dropdown.image
+    static let comment = DesignSystemAsset.Images.comment.image
+    static let person = DesignSystemAsset.Images.person.image
 }
