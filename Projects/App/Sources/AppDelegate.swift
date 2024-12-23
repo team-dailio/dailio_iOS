d87f2d76b467b301203ad7e51737793084aba180
circle0802 <hawon9781@gmail.com>
2024-12-23 19:51:03 +0900

    setting :: 실행시키기 가능해짐

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -1,8 +1,24 @@
-//
-//  main.swift
-//  dailio_iOS
-//
-//  Created by 원이 on 12/12/24.
-//  Copyright © 2024 com.dailio.app. All rights reserved.
-//
+import UIKit
 
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession:
+                     UISceneSession,
+                     options:
+                     UIScene.ConnectionOptions) -> UISceneConfiguration {
+
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+
+}
