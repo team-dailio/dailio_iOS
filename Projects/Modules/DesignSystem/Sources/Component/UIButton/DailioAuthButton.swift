d966af88fb6c87c2bbda496afa522c43befc7cae
circle0802 <hawon9781@gmail.com>
2024-12-29 16:56:24 +0900

    chore :: DailioButton -> DailioAuthButton 으로 수정 및 코드 수정

--- Projects/Modules/DesignSystem/Sources/Component/UIButton/DailioAuthButton.swift
+++ Projects/Modules/DesignSystem/Sources/Component/UIButton/DailioAuthButton.swift
@@ -2,12 +2,12 @@ import UIKit
 import Then
 import SnapKit
 
-public final class DailioButton: UIButton {
-    public init(_ text: String, textColor: UIColor, buttonColor: UIColor) {
+public final class DailioAuthButton: UIButton {
+    public init(_ text: String) {
         super.init(frame: .zero)
         self.setTitle(text, for: .normal)
-        self.tintColor = textColor
-        self.backgroundColor = buttonColor
+        self.tintColor = .white
+        self.backgroundColor = .primary500
         self.layer.cornerRadius = 12
         self.layer.shadowRadius = 8
         self.layer.shadowOpacity = 0.25
