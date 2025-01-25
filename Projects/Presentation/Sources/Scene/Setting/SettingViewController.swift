d37c3d45020c8a01d0a9a59da140d611b660d5e2
circle0802 <hawon9781@gmail.com>
2025-01-25 22:53:26 +0900

    feat :: header에 profile 추가

--- Projects/Presentation/Sources/Scene/Setting/SettingViewController.swift
+++ Projects/Presentation/Sources/Scene/Setting/SettingViewController.swift
@@ -8,8 +8,18 @@ import DesignSystem
 
 public class SettingViewController: BaseViewController<SettingViewModel> {
     private let logoImageView = UIImageView(image: .logoHeader)
+    private let navigateToProfile = UIImageView().then {
+        $0.image = .person.withRenderingMode(.alwaysTemplate)
+        $0.tintColor = .gray300
+        $0.backgroundColor = .gray200
+        $0.layer.cornerRadius = 15
+    }
 
     public override func configureNavigation() {
         self.navigationItem.leftBarButtonItem = .init(customView: logoImageView)
+        self.navigationItem.rightBarButtonItem = .init(customView: navigateToProfile)
+        navigateToProfile.snp.makeConstraints {
+            $0.height.width.equalTo(30)
+        }
     }
 }
